626e81fe4b59791b4f95169935d2174b6e37f773
pashuna-khanal <prashunakhanal8@gmail.com>
2025-12-15 21:12:44 +0545

    Util made to convert real time image data to bitmap

--- app/src/main/java/com/example/fit_buddy/util/ImageUtils.kt
+++ app/src/main/java/com/example/fit_buddy/util/ImageUtils.kt
@@ -1,4 +1,37 @@
 package com.example.fit_buddy.util
 
-class ImageUtils {
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.graphics.YuvImage
+import androidx.camera.core.ImageProxy
+import java.io.ByteArrayOutputStream
+
+fun ImageProxy.toBitmap(): Bitmap? {
+    val yBuffer = planes[0].buffer
+    val uBuffer = planes[1].buffer
+    val vBuffer = planes[2].buffer
+
+    val ySize = yBuffer.remaining()
+    val uSize = uBuffer.remaining()
+    val vSize = vBuffer.remaining()
+
+    val nv21 = ByteArray(ySize + uSize + vSize)
+    yBuffer.get(nv21, 0, ySize)
+    uBuffer.get(nv21, ySize, uSize)
+    vBuffer.get(nv21, ySize + uSize, vSize)
+
+    val yuvImage = YuvImage(nv21, ImageFormat.NV21, width, height, null)
+    val out = ByteArrayOutputStream()
+    yuvImage.compressToJpeg(Rect(0, 0, width, height), 80, out)
+    val imageBytes = out.toByteArray()
+    val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+
+    // Rotate if needed (front camera)
+    val matrix = Matrix()
+    matrix.postRotate(imageInfo.rotationDegrees.toFloat()) // imageInfo from ImageProxy
+    return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
 }
+
