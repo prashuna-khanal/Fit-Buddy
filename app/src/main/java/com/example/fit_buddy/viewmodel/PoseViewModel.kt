c318a6a4e656e04f04811db8c9dbf46932421846
pashuna-khanal <prashunakhanal8@gmail.com>
2025-12-15 21:13:29 +0545

    Pose View model made with necessary final changes

--- app/src/main/java/com/example/fit_buddy/viewmodel/PoseViewModel.kt
+++ app/src/main/java/com/example/fit_buddy/viewmodel/PoseViewModel.kt
@@ -1,4 +1,20 @@
-package com.example.fit_buddy.viewmodel
+package com.example.fitbuddy.viewmodel
 
-class PoseViewModel {
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.fit_buddy.model.PoseModel
+import com.example.fitbuddy.repository.PoseRepo
+
+class PoseViewModel(val repo: PoseRepo) : ViewModel() {
+    var poseState by mutableStateOf<PoseModel?>(null)
+        private set
+
+    fun processFrame(bitmap: Bitmap) {
+        poseState = repo.detectPose(bitmap)
+    }
+
 }
+
