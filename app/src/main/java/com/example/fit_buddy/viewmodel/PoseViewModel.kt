726b3e5fb75833d1e720d0012be256d9cf4f9960
pashuna-khanal <prashunakhanal8@gmail.com>
2025-12-26 21:38:51 +0545

    Pushup and Squats

--- app/src/main/java/com/example/fit_buddy/viewmodel/PoseViewModel.kt
+++ app/src/main/java/com/example/fit_buddy/viewmodel/PoseViewModel.kt
@@ -12,9 +12,44 @@ class PoseViewModel(val repo: PoseRepo) : ViewModel() {
     var poseState by mutableStateOf<PoseModel?>(null)
         private set
 
-    fun processFrame(bitmap: Bitmap) {
-        poseState = repo.detectPose(bitmap)
+    var currentExercise by mutableStateOf(ExerciseType.SQUAT)
+        private set
+
+    var squatCount by mutableStateOf(0)
+        private set
+
+    var pushUpCount by mutableStateOf(0)
+        private set
+
+    enum class ExerciseType {
+        SQUAT, PUSH_UP
     }
 
-}
+    fun processFrame(bitmap: Bitmap) {
+        // This calls detectSquat() or detectPushUp() based on currentExercise
+        poseState = when (currentExercise) {
+            ExerciseType.SQUAT -> repo.detectSquat(bitmap)
+            ExerciseType.PUSH_UP -> repo.detectPushUp(bitmap)
+        }
+        // Update counts from repo
+        squatCount = repo.getSquatCount()
+        pushUpCount = repo.getPushUpCount()
+    }
 
+    fun setExerciseType(exercise: ExerciseType) {
+        currentExercise = exercise
+        // Reset the other exercise when switching
+        when (exercise) {
+            ExerciseType.SQUAT -> repo.resetPushUp()
+            ExerciseType.PUSH_UP -> repo.resetSquat()
+        }
+        poseState = null
+    }
+
+    fun resetCounts() {
+        repo.resetSquat()
+        repo.resetPushUp()
+        squatCount = 0
+        pushUpCount = 0
+    }
+}
