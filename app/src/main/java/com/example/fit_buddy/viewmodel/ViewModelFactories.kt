810bfe9b0adc932d5e077c7d711493487081789f
pashuna-khanal <prashunakhanal8@gmail.com>
2026-01-26 21:28:11 +0545

    supporting factories for all view model

--- app/src/main/java/com/example/fit_buddy/viewmodel/ViewModelFactories.kt
+++ app/src/main/java/com/example/fit_buddy/viewmodel/ViewModelFactories.kt
@@ -1,4 +1,20 @@
 package com.example.fit_buddy.viewmodel
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.fit_buddy.repository.UserRepo
+import com.example.fit_buddy.viewmodel.UserViewModel
 
-class ViewModelFactories {
+class UserViewModelFactory(
+    private val application: Application,
+    private val repository: UserRepo
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+            return UserViewModel(application, repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+    }
 }
